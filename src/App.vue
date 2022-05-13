9a2cd61002c571b16a887bba0fa95918654a9fa1
Caique Araujo <caique@studiopiggly.com.br>
2022-05-13 15:14:27 -0300

    TreeLight component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-	<h1>Hello world</h1>
+	<div class="tree">
+		<tree-light color="red" />
+		<tree-light color="green" />
+		<tree-light color="blue" />
+		<tree-light color="yellow" />
+		<tree-light color="red" />
+		<tree-light color="green" />
+		<tree-light color="blue" />
+		<tree-light color="yellow" />
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import TreeLight from '@/components/TreeLight.vue';
 
 export default defineComponent({
 	name: 'App',
+
+	components: { TreeLight },
 });
 </script>
 
 <style lang="scss">
+@import '@/styles/components/tree.scss';
 </style>
---
+++ src/components/TreeLight.vue
@@ -0,0 +1,82 @@
+<template>
+	<div :class="['light', { 'is-off': !on }]">
+		<div class="circle" :style="getStyles"></div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/runtime-core';
+
+const availableColors = {
+	blue: { h: 178, s: 100, l: 60 },
+	green: { h: 122, s: 100, l: 60 },
+	red: { h: 0, s: 100, l: 60 },
+	yellow: { h: 49, s: 100, l: 60 },
+};
+
+export type TTreeLightColor = keyof typeof availableColors;
+
+export default defineComponent({
+	name: 'TreeLight',
+
+	data() {
+		return {
+			currentColor: availableColors[this.color],
+			fixedSize: ((i: number) => (i >= 0 && i <= 48 ? i : 24))(
+				this.size
+			),
+		};
+	},
+
+	props: {
+		color: {
+			type: String as PropType<TTreeLightColor>,
+			default: 'red',
+			validator: (i: TTreeLightColor) =>
+				Object.keys(availableColors).includes(i),
+		},
+
+		intensity: {
+			type: Number,
+			default: 1,
+		},
+
+		velocity: {
+			type: Number,
+			default: 1,
+		},
+
+		size: {
+			type: Number,
+			default: 24,
+			validator: (i: number) => i >= 0 && i <= 48,
+		},
+
+		on: {
+			type: Boolean,
+			default: true,
+		},
+	},
+
+	computed: {
+		getStyles() {
+			const styles: Record<string, any> = {};
+
+			styles.backgroundColor = `hsl(${this.currentColor.h}, ${
+				this.currentColor.s
+			}%, ${this.currentColor.l * 1}%)`;
+
+			styles.width = `${this.fixedSize / 2}px`;
+			styles.height = `${this.fixedSize}px`;
+
+			styles.boxShadow = `0px 0px 10px ${styles.backgroundColor}`;
+
+			return styles;
+		},
+	},
+});
+</script>
+
+<style lang="scss">
+@import '@/styles/components/tree-light.scss';
+</style>
