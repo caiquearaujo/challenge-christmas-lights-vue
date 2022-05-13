97cda07aaa34dd7c902188092da5e5771735b6c4
Caique Araujo <caique@studiopiggly.com.br>
2022-05-13 17:24:32 -0300

    Animate to curve calculation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,66 @@
 <template>
 	<tree-branch />
+	<div style="margin: 24px auto; font-size: 48px">
+		{{ anim.curve }}
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TreeBranch from '@/components/TreeBranch.vue';
 
+const animate = (k = 50) => {
+	// curve math to y axis as y = a sin((x-h)/b) + k
+	// @see https://www.desmos.com/calculator/litkgx7vkd
+	return 0.5 * Math.sin(-k / 1) + 0.5;
+};
+
 export default defineComponent({
 	name: 'App',
 
 	components: { TreeBranch },
 
+	created() {
+		this.start();
+	},
+
+	unmounted() {
+		this.stop();
+	},
+
 	data() {
 		return {
-			animation: {
-				maxVelocity: 0.01,
-				minVelocity: 0.005,
+			anim: {
+				velocity: 0.005,
+				frame: 0,
+				curve: 0,
+				id: null as null | number,
 			},
 		};
+	},
+
+	methods: {
+		step() {
+			this.anim.frame += this.anim.velocity;
+			this.anim.curve = animate(this.anim.frame);
+
+			requestAnimationFrame(this.step.bind(this));
+		},
+
+		start() {
+			this.anim.id = requestAnimationFrame(this.step.bind(this));
+		},
+
+		stop() {
+			if (!this.anim.id) return;
+
+			cancelAnimationFrame(this.anim.id);
+			this.anim.id = null;
+		},
+
+		velocity(v: number) {
+			return 0.005 + (55 / 1000) * (v / 100);
+		},
 	},
 });
 </script>
